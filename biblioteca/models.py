c575727eceb68f046e6a2a8d31a13a7ddf324d66
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-23 12:53:34 +0100

    modelo de usuario creado

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import AbstractUser
 
-class Libreria(models.Model):
+""" Referencia por si me hace falta algo
+class Biblioteca(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     rating = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(5)])
@@ -10,3 +12,12 @@ class Libreria(models.Model):
 
     def __str__(self):
         return self.title
+"""
+
+class Usuario(AbstractUser):
+    dni = models.CharField(max_length=9)
+    direccion = models.CharField(max_length=50)
+    telefono = models.IntegerField(validators = [MinValueValidator(0)])
+
+    def __str__(self):
+        return self.dni
